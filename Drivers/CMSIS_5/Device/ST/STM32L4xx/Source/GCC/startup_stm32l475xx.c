458e336284e8fb9c453dffe0d96adafba683db23
julien SARRIOT <julien.sarriot@epitech.eu>
2020-01-14 17:46:21 -0500

    Fix compiling with Clion IDE

--- Drivers/CMSIS_5/Device/ST/STM32L4xx/Source/GCC/startup_stm32l475xx.c
+++ Drivers/CMSIS_5/Device/ST/STM32L4xx/Source/GCC/startup_stm32l475xx.c
@@ -84,7 +84,7 @@ void Reset_Handler (void);                /* Reset Handler */
 static uint8_t stack[__STACK_SIZE] __attribute__ ((aligned(8), used, section(".stack")));
 
 #ifndef __HEAP_SIZE
-#define    __HEAP_SIZE   0x00000C00
+#define    __HEAP_SIZE   0x00000200
 #endif
 #if __HEAP_SIZE > 0
 static uint8_t heap[__HEAP_SIZE]   __attribute__ ((aligned(8), used, section(".heap")));
